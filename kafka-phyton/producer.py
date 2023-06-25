d2e3b3b9ac4ff4c6e0bc9c7f0dbbf8ce4640c83e
NicolasTorreblanca <nicolas.torreblanca@usach.cl>
2023-06-25 15:50:01 -0400

    kafka con docker tambien con productor y consumidor

---
+++ kafka-phyton/producer.py
@@ -0,0 +1,13 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda x: dumps(x).encode('utf-8')
+)
+for j in range(10):
+    print("Iteration", j)
+    data = {'counter': j}
+    producer.send('topic_test', value=data)
+    sleep(0.5)
