4a6aff7aa97d504561bf2df5cc4068067d1286cd
NicolasTorreblanca <nicolas.torreblanca@usach.cl>
2023-07-08 23:21:54 -0400

    conexion y guarrdado de datos en mongo db

--- test/consumerAPI.py
+++ test/consumerAPI.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 from json import loads
-from time import sleep
+from pymongo import MongoClient
 
 consumer = KafkaConsumer(
     'topic_test',
@@ -10,8 +10,16 @@ consumer = KafkaConsumer(
     group_id='my-group-id',
     value_deserializer=lambda x: loads(x.decode('utf-8'))
 )
+
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017")
+db = client["mydatabase"]  # Select the database
+collection = db["mycollection"]  # Select the collection
+
 for event in consumer:
     event_data = event.value
+    # Process and save the data to MongoDB
+    collection.insert_one(event_data)
+
     # Do whatever you want
     print(event_data)
-    sleep(0.5)
